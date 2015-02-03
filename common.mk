41211edf77494df407f73efed34c992544da8086
Kevin Mehall <km@kevinmehall.net>
2015-02-02 19:12:55 -0800

    Enable link-time-optimization
    startup code is moved into this repository so `__attribute__ ((used))`
    can be added to prevent it from optimizing out the vector table.

--- common.mk
+++ common.mk
@@ -6,17 +6,17 @@ $(TARGET)_INCLUDE += \
   -I$(ATMEL_PATH)/include \
 
 $(TARGET)_SRC += \
-  $(CMSIS_PATH)/source/gcc/startup_samd21.c \
+  common/startup_samd21.c \
   common/clock.c \
   common/dma.c \
   common/sercom.c \
 
-$(TARGET)_CFLAGS += -Wall --std=gnu99 -Os -g3
+$(TARGET)_CFLAGS += -Wall --std=gnu99 -Os -g3 -flto
 $(TARGET)_CFLAGS += -fdata-sections -ffunction-sections
 $(TARGET)_CFLAGS += -funsigned-char -funsigned-bitfields
 $(TARGET)_CFLAGS += -mcpu=cortex-m0plus -mthumb
 
-$(TARGET)_LDFLAGS += -mcpu=cortex-m0plus -mthumb
+$(TARGET)_LDFLAGS += -mcpu=cortex-m0plus -mthumb -flto
 $(TARGET)_LDFLAGS += -Wl,--gc-sections --specs=nano.specs
 
 $(TARGET)_LDSCRIPT = common/samr21g18a_firmware.ld
