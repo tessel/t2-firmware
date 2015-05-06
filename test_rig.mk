dc7fba3f890d8221cf497c9ca558b7be088c6437
Kevin Mehall <km@kevinmehall.net>
2015-05-06 16:19:03 -0700

    test_rig: Add CMSIS-DAP implementation from Mbed (untested)
    https://github.com/ARMmbed/CMSIS-DAP/tree/master/interface
    from revision: 700cf43501a5849e133d2cd0a0f1302c42d02797

--- test_rig.mk
+++ test_rig.mk
@@ -6,3 +6,6 @@ include usb.mk
 $(TARGET)_SRC += \
   test_rig/main.c \
   test_rig/usb.c \
+  test_rig/DAP.c \
+  test_rig/SW_DP.c \
+  test_rig/dap_hid.c \
---
+++ test_rig/dap_hid.c
@@ -0,0 +1,22 @@
+#include "test_rig.h"
+#include "DAP_config.h"
+#include "DAP.h"
+
+u8 dap_buf_in[DAP_PACKET_SIZE];
+u8 dap_buf_out[DAP_PACKET_SIZE];
+
+void dap_enable() {
+    DAP_Setup();
+    usb_enable_ep(USB_EP_DAP_HID_OUT, USB_EP_TYPE_INTERRUPT, 64);
+    usb_enable_ep(USB_EP_DAP_HID_IN, USB_EP_TYPE_INTERRUPT, 64);
+    usb_ep_start_out(USB_EP_DAP_HID_OUT, dap_buf_out, DAP_PACKET_SIZE);
+}
+
+void dap_handle_usb_in_completion() {
+    usb_ep_start_out(USB_EP_DAP_HID_OUT, dap_buf_out, DAP_PACKET_SIZE);
+}
+
+void dap_handle_usb_out_completion() {
+    DAP_ProcessCommand(dap_buf_out, dap_buf_in);
+    usb_ep_start_in(USB_EP_DAP_HID_IN, dap_buf_in, DAP_PACKET_SIZE, false);
+}
