195fcbf8d7d55cd6bb32e731b0be40b1c72c7e80
Eric Kolker <e.kolker@gmail.com>
2015-05-25 13:58:42 -0700

    average values for 1.2, 1.8

--- test_rig/rig-commander/calibrate_testalator.py
+++ test_rig/rig-commander/calibrate_testalator.py
@@ -26,8 +26,6 @@ def calibrate(testy):
     print 'Calibrating Testalator:\n', #testy.serial
     # calibration = {'serial' : testy.serial}
 
-    # for p in analog_pins:
-    #     calibration[p] = {}
 
     # raw_input('\nClamp in the grounding plate and press ENTER.')
     # print '\nReading initial values at 0V, 0A'
@@ -45,15 +43,32 @@ def calibrate(testy):
 
     calibration = {"VOLTAGE_VREF": {"0": 4092.608, "2.5": 4092.626}, "VOLTAGE_12": {"0": 40.712, "2.5": 43.633}, "VOLTAGE_5VUSB0": {"0": 40.62, "2.5": 2046.812}, "VOLTAGE_5VUSB1": {"0": 40.495, "2.5": 2045.701}, "VOLTAGE_33CP": {"0": 40.122, "2.5": 2045.072}, "VOLTAGE_18": {"0": 40.644, "2.5": 43.738}, "CURRENT_PORTA33": {"0": 15.081, "2.5": 14.997}, "VOLTAGE_PORTB33": {"0": 40.537, "2.5": 2046.906}, "CURRENT_USB0": {"0": 15.485, "2.5": 15.239}, "CURRENT_USB1": {"0": 15.315, "2.5": 18.036}, "CURRENT_UUT": {"0": 21.458, "2.5": 21.335}, "VOLTAGE_PORTA33": {"0": 41.344, "2.5": 2047.37}, "serial": "KDYXL8WFQ98SJP9HD0K493YPYS", "VOLTAGE_5VUUT": {"0": 40.907, "2.5": 2046.387}, "VOLTAGE_33MT": {"0": 40.761, "2.5": 2046.685}, "CURRENT_PORTB33": {"0": 15.507, "2.5": 23.523}}
 
+    avg = 0.0
     for p in analog_pins:
         c = calibration[p]
-        c['b'] = -c['0']
-        if 'VOLTAGE' in p:
-            c['m'] = (c['2.5'] - c['0']) * 1.0 / ADC_MAX_VALUE * (2.0 if p in divided_pins else 1.0)
+
+        # no voltage divider for current or 1.2V, 1.8V, or 2.5V reference inputs
+        c['div'] = (1.0 if (p[0] == 'C' or p[7:] in ['_12', '_18', '_VREF']) else 2.0)
+
+        c['b'] = -c['0'] if 'VREF' not in p else 0.0
+        
+        if 'VOLTAGE' in p and p[7:] not in ['_12', '_18', '_VREF']:
+            c['m'] = (ADC_MAX_VALUE * 1.0 / (c['2.5'] - c['0']) * 1.0) / c['div']
+            avg = avg + c['m']
         else:   
             c['m'] = 1.0
-        print p, c['b'], c['m']#counts_to_volts(testy.analog(p) * c['m'] + c['b'])
-    
+
+    avg = avg / 7
+    calibration['VOLTAGE_12']['m'] = avg
+    calibration['VOLTAGE_18']['m'] = avg
+
+    print avg
+
+    for p in analog_pins:
+        c = calibration[p]
+        print p, c['b'], c['m'], c['div']#counts_to_volts(testy.analog(p) * c['m'] + c['b'])
+
+
     print 'Done!'
 
 def measure_analog_pin(pin):
